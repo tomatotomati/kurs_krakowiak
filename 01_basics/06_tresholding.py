630213a9a1f539e7d97b36b906bc4663ddba47eb
tomatotomati <brzostekmateusz98@gmail.com>
2022-07-11 20:13:34 +0200

    add basics

---
+++ 01_basics/06_tresholding.py
@@ -0,0 +1,17 @@
+import cv2
+
+img = cv2.imread(r'assets\grey.png', 0)
+print(img)
+cv2.imshow('img', img)
+
+# thresh_binary = cv2.threshold(src=img, thresh=150, maxval=255, type=cv2.THRESH_BINARY)[1]
+# cv2.imshow('thresh_binary', thresh_binary)
+# cv2.waitKey(0)
+
+for thresh in [0, 50, 100, 150, 200]:
+    thresh_binary = cv2.threshold(src=img, thresh=thresh, maxval=255, type=cv2.THRESH_BINARY)[1]
+    cv2.imshow(f'thresh_binary: {thresh}', thresh_binary)
+    cv2.waitKey(2000)
+    cv2.destroyWindow(f'thresh_binary: {thresh}')
+
+cv2.waitKey(0)
---
+++ 01_basics/08_contours.py
@@ -0,0 +1,38 @@
+import cv2
+
+original_img = cv2.imread(r'assets\python.png')
+img = original_img.copy()
+# cv2.imshow('original_img', img)
+
+gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+# cv2.imshow('gray', gray)
+
+thresh = cv2.threshold(src=gray, thresh=250, maxval=255, type=cv2.THRESH_BINARY)[1]
+# cv2.imshow('thresh', thresh)
+
+contours = cv2.findContours(image=thresh, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)[0]
+print(f'[INFO] Liczba wszystkich konturów: {len(contours)}')
+
+# img_cnt = cv2.drawContours(image=img.copy(), contours=[contours[4]], contourIdx=-1, color=(0, 255, 0), thickness=2)
+# cv2.imshow('img_cnt', img_cnt)
+
+# for i in range(len(contours)):
+#     img_cnt = cv2.drawContours(image=img.copy(), contours=[contours[i]], contourIdx=-1, color=(0, 255, 0), thickness=2)
+#     cv2.imshow('img_cnt', img_cnt)
+#     cv2.waitKey(0)
+
+max_area = 0
+for idx, contour in enumerate(contours):
+    area = cv2.contourArea(contour=contour, oriented=True)
+    if area > max_area:
+        max_area = area
+        idx_flag_area = idx
+
+print(f'[INFO] Indeks konturu o największym polu: {idx_flag_area}\nPole: {max_area}')
+
+img_cnt_max_area = cv2.drawContours(image=img.copy(), contours=[contours[idx_flag_area]], contourIdx=-1, color=(0, 255, 0), thickness=2)
+cv2.imshow('img_cnt_max_area', img_cnt_max_area)
+cv2.waitKey(0)
+
+parameter = cv2.arcLength(curve=contours[idx_flag_area], closed=True)
+print(parameter)
